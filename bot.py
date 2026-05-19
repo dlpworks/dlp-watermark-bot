5f0166f0e2712bd044e3723a06947ac7544db589
dlpworks <dlp.works.pro@gmail.com>
2026-05-19 12:37:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ WATERMARK_OPACITY    = 0                # 0=invisible, 100=opaque (recommandé: 
 WATERMARK_FONT_SIZE  = 80               # Taille du texte watermark
 
 LOGO_FILE            = "logo.png"        # Fichier de ton logo
-LOGO_SIZE_PERCENT    = 12               # Taille du logo en % de la largeur de l'image
+LOGO_SIZE_PERCENT    = 11               # Taille du logo en % de la largeur de l'image
 LOGO_MARGIN          = 20               # Marge depuis le bord (pixels)
 
 OUTPUT_QUALITY       = 100               # Qualité JPEG export (95-100 pour HDR)
